984258116e58555d6429a8b2f8f34dfc5b671edf
yoshiki.emori <yoshiki.wgtc.0125@gmail.com>
2020-09-14 17:55:13 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,37 +1,34 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks.order(id: :desc)
   end
   
   def show
-    @task = Task.find(params[:id])
   end
   
   def new
-    @task = Task.new
+    @task = current_user.tasks.build # form_with 用
   end
 
   def create
-    @task = Task.new(task_params)
-    
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'Task が正常に作成されました'
       redirect_to @task
     else
+      @tasks = current_user.tasks.order(id: :desc)
       flash.now[:danger] = 'Task が作成されませんでした'
       render :new
     end
   end
   
   def edit
-    @task = Task.find(params[:id])
   end
   
   def update
-    @task = Task.find(params[:id])
-    
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
@@ -42,11 +39,9 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
-    
     flash[:success]= 'Task は正常に削除されました'
-    redirect_to tasks_url
+    redirect_back(fallback_location: root_url)
   end
   
   private
@@ -54,5 +49,12 @@ class TasksController < ApplicationController
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
